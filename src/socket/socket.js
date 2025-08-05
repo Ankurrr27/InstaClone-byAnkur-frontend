75402f4be4d09d61d50c0a03a8502521363ca644
Ankur <ankurp22singh@gmail.com>
2025-08-05 21:23:53 +0530

    update

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -4,7 +4,7 @@ import { io } from "socket.io-client";
 let socket = null;
 
 export const connectSocket = (userId) => {
-  socket = io("http://localhost:8000", {
+  socket = io("https://insta-clone-by-ankur-backend.vercel.app", {
     query: { userId },
     withCredentials: true,
   });
