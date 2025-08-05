65101a484cc192f8a205dd045d259b5f803e89c7
Ankur <ankurp22singh@gmail.com>
2025-08-06 00:13:12 +0530

    update

--- src/hooks/userGetAllPost.jsx
+++ src/hooks/userGetAllPost.jsx
@@ -8,7 +8,11 @@ const useGetAllPost = () => {
     const dispatch = useDispatch();
     useEffect(() => {
         const fetchAllPost = async () => {
+            console.log("Fetched posts:", res.data); // should show array of posts
+
             try {
+               console.log("Fetched posts:", res.data); // should show array of posts
+
                 const res = await axios.get('https://instaclone-byankur-backend.onrender.com/api/v1/post/all', { withCredentials: true });
                 if (res.data.success) { 
                     dispatch(setPosts(res.data.posts));
