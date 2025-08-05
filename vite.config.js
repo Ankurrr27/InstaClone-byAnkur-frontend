775530083731d49a574db027c00575056734b737
Ankur <ankurp22singh@gmail.com>
2025-08-05 22:30:20 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,20 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
- 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    proxy: {
+      '/socket.io': {
+        target: 'http://localhost:8000', // your backend server
+        ws: true, // enable websocket proxying
+      },
     },
   },
 })
