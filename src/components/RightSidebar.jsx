1aa76f515955c5e7107126d61c6add7f5467db86
Ankur <ankurp22singh@gmail.com>
2025-08-06 16:45:50 +0530

    update

--- src/components/RightSidebar.jsx
+++ src/components/RightSidebar.jsx
@@ -12,7 +12,7 @@ const RightSidebar = () => {
 
 
   return (
-  <div className='border-l-[1px] pl-6 border-gray-300 w-80 fixed right-0 top-0 h-full overflow-hidden'>
+  <div className='border-l-[1px] pl-6 border-gray-300 w-80 right-0 top-0 h-full overflow-hidden'>
     <div className=' w-80 my-10 pr-20 '>
       <div className='flex items-center gap-2'>
         <Link to={`/profile/${user?._id}`}>
