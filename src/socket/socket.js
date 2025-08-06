5970bd3b0c18d19ad52236ef0e99b9f2b02db64d
Ankur <ankurp22singh@gmail.com>
2025-08-06 08:58:21 +0530

    update

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -4,7 +4,7 @@ import { io } from "socket.io-client";
 let socket = null;
 
 export const connectSocket = (userId) => {
-  socket = io("https://instaclone-byankur-backend.onrender.com/", {
+  socket = io("https://instaclone-byankur-backend.onrender.com", {
     query: { userId },
 
     withCredentials: true,
