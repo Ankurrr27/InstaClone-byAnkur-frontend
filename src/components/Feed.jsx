cf90a2cd6daf701042da3fbc7cbf13656879ba25
Ankur <ankurp22singh@gmail.com>
2025-08-06 16:26:00 +0530

    update

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -6,7 +6,8 @@ const Feed = () => {
     <div className="flex-1 my-8 flex flex-col items-center">
       <div className="w-full max-w-2xl">
         <p className="text-sm text-black italic mb-4 text-center">
-          This is the desktop version & Some features are not working here.
+         This is the desktop version & some features might not work properly.
+         Also, avoid refreshing the page while testing — it may lose connection
         </p>
         <Posts />
       </div>
