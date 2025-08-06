bcaa00c09e75c4e03df284e925e7f4ad4fff5405
Ankur <ankurp22singh@gmail.com>
2025-08-06 10:29:35 +0530

    update

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -3,12 +3,13 @@ import Posts from "./Posts";
 
 const Feed = () => {
   return (
-    <div className="flex-1 my-8 flex flex-col items-center pl-[20%] pr-[20%]">
-      <p className="text-sm text-black italic mb-4">
-        This is the desktop version & Some features are not working here .
-      </p>
-
-      <Posts />
+    <div className="flex-1 my-8 flex flex-col items-center">
+      <div className="w-full max-w-2xl">
+        <p className="text-sm text-black italic mb-4 text-center">
+          This is the desktop version & Some features are not working here.
+        </p>
+        <Posts />
+      </div>
     </div>
   );
 };
