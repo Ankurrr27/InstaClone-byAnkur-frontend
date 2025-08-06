338f6010c42e373b797577d0e70b07c8d1de0bda
Ankur <ankurp22singh@gmail.com>
2025-08-06 08:49:40 +0530

    update

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import Posts from './Posts'
+import React from "react";
+import Posts from "./Posts";
 
 const Feed = () => {
   return (
-    <div className='flex-1 my-8 flex flex-col items-center pl-[20%] pr-[20%]'>
-        <p className="text-sm text-black italic mb-4">
-  This is the desktop version & Some features are not working here .
-</p>
+    <div className="flex-1 my-8 flex flex-col items-center pl-[20%] pr-[20%]">
+      <p className="text-sm text-black italic mb-4">
+        This is the desktop version & Some features are not working here .
+      </p>
 
-        <Posts/>
+      <Posts />
     </div>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
