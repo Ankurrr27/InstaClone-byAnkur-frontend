b4296dc5adf56ed503500114bab637fdb6543ac3
Ankur <ankurp22singh@gmail.com>
2025-08-06 09:07:56 +0530

    update

--- src/hooks/useGetUserProfile.jsx
+++ src/hooks/useGetUserProfile.jsx
@@ -3,23 +3,32 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 
-
 const useGetUserProfile = (userId) => {
-    const dispatch = useDispatch();
-    // const [userProfile, setUserProfile] = useState(null)
-    useEffect(() => {
-        const fetchUserProfile = async () => {
-            try {
-                
-                const res = await axios.get(`https://instaclone-byankur-backend.onrender.com/user/${userId}/profile`, { withCredentials: true });
-                if (res.data.success) { 
-                    dispatch(setUserProfile(res.data.user));
-                }
-            } catch (error) {
-                console.log(error);
-            }
+  const dispatch = useDispatch();
+  const [userProfile, setUserProfile] = useState(null);
+
+  useEffect(() => {
+    if (!userId) return;
+
+    const fetchUserProfile = async () => {
+      try {
+        const res = await axios.get(
+          `https://instaclone-byankur-backend.onrender.com/user/${userId}/profile`,
+          { withCredentials: true }
+        );
+        if (res.data.success) {
+          setUserProfile(res.data.user);
+          dispatch(setUserProfile(res.data.user));
         }
-        fetchUserProfile();
-    }, [userId]);
+      } catch (error) {
+        console.error("Failed to fetch user profile:", error);
+      }
+    };
+
+    fetchUserProfile();
+  }, [userId, dispatch]);
+
+  return userProfile;
 };
+
 export default useGetUserProfile;
