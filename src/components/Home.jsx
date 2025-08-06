32a3e614e5d93fc1a9c4be2c811bc7c23baf11c8
Ankur <ankurp22singh@gmail.com>
2025-08-06 22:19:29 +0530

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import Feed from './Feed';
-import { Outlet } from 'react-router-dom';
-import RightSidebar from './RightSidebar';
-import useGetAllPost from '../hooks/userGetAllPost';
-import useGetSuggestedUsers from '@/hooks/useGetSuggestedUsers';
+import React from 'react'
+import Feed from './Feed'
+import { Outlet } from 'react-router-dom'
+import RightSidebar from './RightSidebar'
+import useGetAllPost from '../hooks/userGetAllPost'
+import useGetSuggestedUsers from '@/hooks/useGetSuggestedUsers'
 
 const Home = () => {
     useGetAllPost();
     useGetSuggestedUsers();
-
     return (
-        <div className="flex h-screen overflow-hidden">
-            <div className="flex-grow overflow-y-scroll">
+        <div className='flex'>
+            
+            <div className='flex-grow'>
                 <Feed />
                 <Outlet />
             </div>
-            <div className="w-[300px] overflow-y-scroll h-screen">
-                <RightSidebar />
-            </div>
+            <RightSidebar />
         </div>
-    );
-};
+    )
+}
 
-export default Home;
+export default Home
