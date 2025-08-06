68a5b4e9c991c2805294f3c14321a2bbcdd759a2
Ankur <ankurp22singh@gmail.com>
2025-08-06 17:05:44 +0530

    update

--- src/components/RightSidebar.jsx
+++ src/components/RightSidebar.jsx
@@ -7,30 +7,29 @@ import SuggestedUsers from './SuggestedUsers';
 const RightSidebar = () => {
   const { user } = useSelector(store => store.auth);
   
-  if (location.pathname.startsWith("/profile")) return null;
-  if (location.pathname.startsWith("/chat")) return null;
+   if (location.pathname.startsWith("/profile")) return null;
+   else if (location.pathname.startsWith("/chat")) return null;
+
 
   return (
-    <div className='border-l-[1px] pl-6 border-gray-300 w-80 right-0 top-0 float h-full overflow-y-auto'>
-      <div className='w-80 my-10 pr-20'>
-        <div className='flex items-center gap-2'>
-          <Link to={`/profile/${user?._id}`}>
-            <Avatar>
-              <AvatarImage src={user?.profilePicture} alt="post_image" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-          </Link>
-          <div>
-            <h1 className='font-semibold text-sm'>
-              <Link to={`/profile/${user?._id}`}>{user?.username}</Link>
-            </h1>
-            <span className='text-gray-600 text-sm'>{user?.bio || 'Bio here...'}</span>
-          </div>
+  <div className='border-l-[1px] pl-6  border-gray-300 w-80 fixed right-0 top-0 h-full '>
+    <div className=' w-80 my-10 pr-20 '>
+      <div className='flex items-center gap-2'>
+        <Link to={`/profile/${user?._id}`}>
+          <Avatar>
+            <AvatarImage src={user?.profilePicture} alt="post_image" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </Link>
+        <div>
+          <h1 className='font-semibold text-sm'><Link to={`/profile/${user?._id}`}>{user?.username}</Link></h1>
+          <span className='text-gray-600 text-sm'>{user?.bio || 'Bio here...'}</span>
         </div>
-        <SuggestedUsers />
       </div>
+      <SuggestedUsers/>
+    </div>
     </div>
   )
 }
 
-export default RightSidebar;
+export default RightSidebar
