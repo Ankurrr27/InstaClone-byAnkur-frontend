e3d4f38bb4ea409f8fe01492ea58d4fa702d4331
Ankur <ankurp22singh@gmail.com>
2025-08-08 09:10:17 +0530

    add

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -3,7 +3,7 @@ import Posts from "./Posts";
 
 const Feed = () => {
   return (
-    <div className="flex-1 my-8 flex flex-col items-center p-12">
+    <div className="flex-1 my-8 flex flex-col items-center pr-12">
       <div className="w-full max-w-2xl">
         <p className="text-sm text-black italic mb-4 text-center">
          This is the desktop version & some features might not work properly.
